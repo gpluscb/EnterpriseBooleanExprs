75c72f513632c524e813000d33f3725ce565395e
MarRue <20143778+gpluscb@users.noreply.github.com>
2023-08-22 22:16:50 +0200

    add all and none exprs

---
+++ src/main/java/com/github/gpluscb/boolean_expression/AllBooleanExpression.java
@@ -0,0 +1,22 @@
+package com.github.gpluscb.boolean_expression;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class AllBooleanExpression implements BooleanExpression {
+    private Stream<BooleanExpression> exprs;
+
+    public AllBooleanExpression(List<BooleanExpression> exprs) {
+        this.exprs = Objects.requireNonNull(exprs).stream();
+    }
+
+    public AllBooleanExpression(Stream<BooleanExpression> exprs) {
+        this.exprs = Objects.requireNonNull(exprs);
+    }
+
+    @Override
+    public boolean evaluate() {
+        return exprs.allMatch(BooleanExpression::evaluate);
+    }
+}
---
+++ src/main/java/com/github/gpluscb/boolean_expression/NoneBooleanExpression.java
@@ -0,0 +1,22 @@
+package com.github.gpluscb.boolean_expression;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class NoneBooleanExpression implements BooleanExpression {
+    private Stream<BooleanExpression> exprs;
+
+    public NoneBooleanExpression(List<BooleanExpression> exprs) {
+        this.exprs = Objects.requireNonNull(exprs).stream();
+    }
+
+    public NoneBooleanExpression(Stream<BooleanExpression> exprs) {
+        this.exprs = Objects.requireNonNull(exprs);
+    }
+
+    @Override
+    public boolean evaluate() {
+        return exprs.noneMatch(BooleanExpression::evaluate);
+    }
+}
